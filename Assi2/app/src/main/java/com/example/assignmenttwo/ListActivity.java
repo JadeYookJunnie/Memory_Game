7b1f2234db286c82a89d91d745c3781763bfe468
School <jt233@students.waikato.ac.nz>
2022-09-27 00:16:46 +1300

    customadapter + listview

--- Assi2/app/src/main/java/com/example/assignmenttwo/ListActivity.java
+++ Assi2/app/src/main/java/com/example/assignmenttwo/ListActivity.java
@@ -2,7 +2,16 @@ package com.example.assignmenttwo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -12,10 +21,36 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
     }
 
+
     public class Card {
         public int number;
         public String faceUp;
         public String faceDown;
         public boolean match;
+    }
+
+    //generate random list of cards
+    //start with basic manual entries for testing
+
+    //customadapter
+    public class CustomAdapter extends ArrayAdapter<Card> {
+         Context context;
+         ArrayList<Card> cards;
+
+         public CustomAdapter(Context context, ArrayList<Card> cards) {
+             super(context, R.layout.row_listview, cards);
+             this.context = context;
+             this.cards = cards;
+         }
+
+         public View getView(int position, View convertView, ViewGroup parent) {
+             Card current = cards.get(position);
+
+             LayoutInflater inflater = LayoutInflater.from(getContext());
+             convertView = inflater.inflate(R.layout.row_listview,parent,false);
+             TextView textView = (TextView) convertView.findViewById(R.id.rowText);
+             textView.setText("card " + current.number);
+             return convertView;
+         }
     }
 }
