7790ece670cf24ce75fd4a02fda60e7d42ed955b
School <jt233@students.waikato.ac.nz>
2022-09-26 23:12:53 +1300

    card added listactivity functioning

--- Assi2/app/src/main/java/com/example/assignmenttwo/MainActivity.java
+++ Assi2/app/src/main/java/com/example/assignmenttwo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.assignmenttwo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void activateList(View v) {
+        Button button = findViewById(R.id.startButton);
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
