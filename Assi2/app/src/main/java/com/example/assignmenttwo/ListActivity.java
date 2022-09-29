ca50895ae0fa0ff54f3dc9ba8645df0e21f6c565
School <jt233@students.waikato.ac.nz>
2022-09-29 22:34:52 +1300

    cards displaying list generated randomly

--- Assi2/app/src/main/java/com/example/assignmenttwo/ListActivity.java
+++ Assi2/app/src/main/java/com/example/assignmenttwo/ListActivity.java
@@ -1,8 +1,13 @@
 package com.example.assignmenttwo;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import java.lang.reflect.Array;
+import java.util.*;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,14 +21,16 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
 
     //generate random list of cards
     //start with basic manual entries for testing
-    Card card1 = new Card(1,"img_card_1.png");
-    Card card2 = new Card(2, "img_card_2.png");
-    ArrayList<Card> cards = new ArrayList<>(Arrays.asList(card1, card2));
+//    Card card1 = new Card(1,"img_card_1");
+//    Card card2 = new Card(2, "img_card_2");
+    //ArrayList<Card> cards = new ArrayList<>(Arrays.asList(card1, card2));
+    ArrayList<Card> cards = generate();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +40,48 @@ public class ListActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.cardList);
         CustomAdapter adapterCustom = new CustomAdapter(this, cards);
         listView.setAdapter(adapterCustom);
+    }
+
+    //@RequiresApi(api = Build.VERSION_CODES.N)
+    public ArrayList<Card> generate() {
+        ArrayList<Card> Cardlist = new ArrayList<>();
+//        String[] names = {"img_card_1", "img_card_1", "img_card_2", "img_card_2",
+//                "img_card_3", "img_card_3", "img_card_4", "img_card_4", "img_card_5",
+//                "img_card_5", "img_card_6", "img_card_6"};
+        ArrayList<String> names = new ArrayList<>();
+//        Cardlist.add(new Card("img_card_1"));
+//        Cardlist.add(new Card("img_card_1"));
+//        Cardlist.add(new Card("img_card_2"));
+//        Cardlist.add(new Card("img_card_2"));
+//        Cardlist.add(new Card("img_card_3"));
+//        Cardlist.add(new Card("img_card_3"));
+//        Cardlist.add(new Card("img_card_4"));
+//        Cardlist.add(new Card("img_card_4"));
+//        Cardlist.add(new Card("img_card_5"));
+//        Cardlist.add(new Card("img_card_5"));
+//        Cardlist.add(new Card("img_card_6"));
+//        Cardlist.add(new Card("img_card_6"));
+        names.add("img_card_1");
+        names.add("img_card_1");
+        names.add("img_card_2");
+        names.add("img_card_2");
+        names.add("img_card_3");
+        names.add("img_card_3");
+        names.add("img_card_4");
+        names.add("img_card_4");
+        names.add("img_card_5");
+        names.add("img_card_5");
+        names.add("img_card_6");
+        names.add("img_card_6");
+
+//        ArrayList<Integer> indexes = new ArrayList<Integer>();
+        Collections.shuffle(names);
+        for(int i = 1; i <= 5; i++) {
+            Cardlist.add(new Card(i, names.get(i)));
+        }
+        return Cardlist;
+
+
     }
 
 
@@ -48,12 +97,6 @@ public class ListActivity extends AppCompatActivity {
             faceDown = "img_card_facedown.png";
             match = false;
 
-            //move when match is found
-            //ImageView card = (ImageView) findViewById(R.id.rowImg);
-//            int img_id = getResources().getIdentifier(faceUp, "drawable", getPackageName());
-//            Drawable drawable =getDrawable(img_id);
-//            card.setImageDrawable(drawable);
-            //card.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
         }
     }
@@ -76,7 +119,16 @@ public class ListActivity extends AppCompatActivity {
 
              LayoutInflater inflater = LayoutInflater.from(getContext());
              convertView = inflater.inflate(R.layout.row_listview,parent,false);
+             ImageView imageView = (ImageView) convertView.findViewById(R.id.rowImg);
              TextView textView = (TextView) convertView.findViewById(R.id.rowText);
+//             int id = getContext().getResources().getIdentifier(current.faceUp, "drawable", getContext().getPackageName());
+//             imageView.setImageResource(id);
+             String up = current.faceUp;
+             int id = getResources().getIdentifier(up, "drawable", getPackageName());
+             //error here
+             Drawable drawable = getDrawable(id);
+//
+             imageView.setBackground(drawable);
              textView.setText("card " + current.number);
              return convertView;
          }
